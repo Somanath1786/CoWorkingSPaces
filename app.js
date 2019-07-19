6dbf3e958b2af72c3cc6224d9d5cf6843cc29dbf
Somanath Krishnaswamy <soma17@uw.edu>
2019-07-19 16:45:24 -0700

    Added MongoDB connection and set up the first basic route without any schema

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const { NODE_ENV, PORT } = process.env
+const { NODE_ENV, PORT, MONGO_DB_CONNECTION } = process.env
 const express = require('express');
+const mongoose = require('mongoose')
+
 const app = express();
 
 if (NODE_ENV === "development")
 {
     app.use(require('morgan')('dev'));
+}
+
+// Set up a Mong DB database connection here
+if (MONGO_DB_CONNECTION)
+{
+    mongoose.connect(MONGO_DB_CONNECTION, {useNewUrlParser: true})
+    console.log('Connected to DataBase...');
+}
+else
+{
+    console.log('Unable to connect to database');
 }
 
 // In case of error, set up the error handler to console log the message to the developer
@@ -17,6 +30,10 @@ app.use((err, req, res, next) =>{
     const { message, status } = err;
     res.status(status).json({status, message})
 })
+
+// Set up the starter routes
+app.use(require('body-parser').json());
+app.use('/api/units', require('./api/routes/units'))
 
 // Set up a listener to listen on the specified PORT
 const listener = () => console.log('Listening on port ' + PORT);
--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ Till I reach that stage, this readMe is a readME only for ME :)
 2. Download and install all the necessary packages required to set up a server
 3. Write code just enough to run a server that listens on a particular port
 4. Commit this code
+
+5. Set up a connection to MongoDB and validate that you can connect to the same
+6. Set up a basic route without any schema and make sure your first get request returns a hardcoded message
+7. Commit
